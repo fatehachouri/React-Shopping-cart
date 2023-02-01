0e542e4488585356cf2e1352097dd0b3ab204167
abdel fatah achouri <fatehachouri@outlook.fr>
2023-02-01 18:47:23 +0100

    third

--- src/components/ProductListItem.jsx
+++ src/components/ProductListItem.jsx
@@ -8,7 +8,7 @@ export default function ProductListItem({product}) {
     return (
     <div className="col-md-4 mb-2">
         <div className="card" style={{width: "18rem"}}>
-  <img src={product.image} className="card-img-top" alt="..." />
+  <img src={product.image} className="card-img-top" alt="..." style={{width:"100%", height:"200px"}} />
   <div className="card-body">
     <h5 className="card-title">{product.name}</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
